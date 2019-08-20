9e3caaa37a23dc8e59b2571abdff3dcbe5782a3c
unknown <arian.angoma@tecsup.edu.pe>
2019-08-19 21:52:16 -0500

    first commit

---
+++ Caso03/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Caso03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CobAnios = new System.Windows.Forms.ComboBox();
+            this.CobMes = new System.Windows.Forms.ComboBox();
+            this.DgDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CobAnios
+            // 
+            this.CobAnios.FormattingEnabled = true;
+            this.CobAnios.Location = new System.Drawing.Point(13, 13);
+            this.CobAnios.Name = "CobAnios";
+            this.CobAnios.Size = new System.Drawing.Size(138, 21);
+            this.CobAnios.TabIndex = 0;
+            this.CobAnios.SelectionChangeCommitted += new System.EventHandler(this.CobAnios_SelectionChangeCommitted);
+            // 
+            // CobMes
+            // 
+            this.CobMes.FormattingEnabled = true;
+            this.CobMes.Location = new System.Drawing.Point(217, 13);
+            this.CobMes.Name = "CobMes";
+            this.CobMes.Size = new System.Drawing.Size(138, 21);
+            this.CobMes.TabIndex = 1;
+            this.CobMes.SelectionChangeCommitted += new System.EventHandler(this.CobMes_SelectionChangeCommitted);
+            // 
+            // DgDatos
+            // 
+            this.DgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgDatos.Location = new System.Drawing.Point(13, 69);
+            this.DgDatos.Name = "DgDatos";
+            this.DgDatos.Size = new System.Drawing.Size(775, 369);
+            this.DgDatos.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DgDatos);
+            this.Controls.Add(this.CobMes);
+            this.Controls.Add(this.CobAnios);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CobAnios;
+        private System.Windows.Forms.ComboBox CobMes;
+        private System.Windows.Forms.DataGridView DgDatos;
+    }
+}
+
---
+++ Caso03/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Caso03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Caso03"].ConnectionString);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListarComboAnios();
+        }
+
+        public void ListarComboAnios()
+        {
+            using (SqlCommand command = new SqlCommand("Usp_anios", cn))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    CobAnios.DataSource = dataTable;
+                    CobAnios.DisplayMember = "Years";
+                    CobAnios.ValueMember = "order_years";
+                }
+            }
+        }
+
+        private void CobAnios_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int codigo;
+            codigo = Convert.ToInt32(CobAnios.SelectedValue);
+            using (SqlCommand command = new SqlCommand("Usp_mes_02", cn))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderYear", codigo);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    CobMes.DataSource = dataTable;
+                    CobMes.DisplayMember = "month_name";
+                    CobMes.ValueMember = "month_number";
+                }
+            }
+        }
+
+        private void CobMes_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int year, month;
+            year = Convert.ToInt32(CobMes.SelectedValue);
+            month = Convert.ToInt32(CobMes.SelectedValue);
+            using (SqlCommand command = new SqlCommand("Usp_filtrar_anio_mes", cn))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderYear", year);
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderMonth", month);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    DgDatos.DataSource = dataTable;
+                }
+            }
+        }
+    }
+}
